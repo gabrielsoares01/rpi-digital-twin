f6f0ff4915b538b4ae4b8300a7647f2d0d67c47f
pedrocroitor <pedrohalk123@gmail.com>
2026-07-30 14:52:03 -0300

    move o iniciar_wifi.sh para a pasta backend e retira a parte de iniciar a main

--- backend/iniciar_wifi.sh
+++ backend/iniciar_wifi.sh
@@ -5,9 +5,5 @@ sudo nmcli device wifi hotspot ssid Robo-Network password robopassword ifname wl
 
 echo "----------------------------------------"
 echo "✅ Rede 'Robo-Network' criada com sucesso!"
-echo "📌 O IP da Raspberry Pi é:"
-echo "📌 IP do Robô:" $(hostname -I | awk '{print $1}')
+echo "📌 O IP da Raspberry Pi é: $(hostname -I | awk '{print $1}')"
 echo "----------------------------------------"
-echo "⚡ Iniciando a telemetria do robô..."
-
-python3 main.py
